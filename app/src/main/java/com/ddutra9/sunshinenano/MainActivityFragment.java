71d38a916d26503a158c3a5460a9764d21bc3bde
donato <ddutra9@gmail.com>
2017-05-22 11:23:53 -0300

    criando arraylist

--- app/src/main/java/com/ddutra9/sunshinenano/MainActivityFragment.java
+++ app/src/main/java/com/ddutra9/sunshinenano/MainActivityFragment.java
@@ -1,15 +1,31 @@
 package com.ddutra9.sunshinenano;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private String[] data = {
+            "Mon 6/23 - Sunny - 31/17",
+            "Tue 6/24 - Foggy - 21/8",
+            "Wed 6/25 - Cloudy - 22/17",
+            "Thurs 6/26 - Rainy - 18/11",
+            "Fri 6/27 - Foggy - 21/10",
+            "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+            "Sun 6/29 - Sunny - 20/7"
+    };
 
     public MainActivityFragment() {
     }
@@ -18,5 +34,12 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ListView listForeCast = (ListView)view.findViewById(R.id.listview_forecast);
+        List<String> weekList = new ArrayList<String>(Arrays.asList(data));
     }
 }

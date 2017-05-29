5397a0a60149352a81066490e6dd7886b902e1ca
ddutra9 <7Free787>
2017-05-28 22:49:12 -0300

    craindo e chamando detail activity

---
+++ app/src/main/java/com/ddutra9/sunshinenano/DetailActivity.java
@@ -0,0 +1,37 @@
+package com.ddutra9.sunshinenano;
+
+import android.app.Fragment;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.LoaderManager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public static class DetailFragment extends Fragment {
+
+        public DetailFragment() {
+
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_detail, container, false);
+        }
+    }
+}

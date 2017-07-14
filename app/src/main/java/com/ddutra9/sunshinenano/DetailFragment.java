b1b3b526fac5d063d7bb48ac5ec6d3f8b2428f7c
donato <ddutra9@gmail.com>
2017-07-14 11:31:05 -0300

    mudando layout do detailfragment

---
+++ app/src/main/java/com/ddutra9/sunshinenano/DetailFragment.java
@@ -0,0 +1,144 @@
+package com.ddutra9.sunshinenano;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.ddutra9.sunshinenano.data.WeatherContract;
+
+/**
+ * Created by donato on 14/07/17.
+ */
+
+public class DetailFragment extends Fragment  implements LoaderManager.LoaderCallbacks<Cursor>{
+    private TextView detailText;
+    private static final int LOADER_DETAIL = 0;
+
+    private static final String[] FORECAST_COLUMNS = {
+            WeatherContract.WeatherEntry.TABLE_NAME + "." + WeatherContract.WeatherEntry._ID,
+            WeatherContract.WeatherEntry.COLUMN_DATE,
+            WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
+            WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
+            WeatherContract.WeatherEntry.COLUMN_MIN_TEMP
+    };
+
+    static final int COL_WEATHER_ID = 0;
+    static final int COL_WEATHER_DATE = 1;
+    static final int COL_WEATHER_DESC = 2;
+    static final int COL_WEATHER_MAX_TEMP = 3;
+    static final int COL_WEATHER_MIN_TEMP = 4;
+
+    private static final String LOG_TAG = DetailFragment.class.getSimpleName();
+
+    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+    private String mForecastStr;
+    private ShareActionProvider mShareActionProvider;
+
+    public DetailFragment() {
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.detail_fragment, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+
+        mShareActionProvider = (ShareActionProvider)
+                MenuItemCompat.getActionProvider(menuItem);
+
+        if(mForecastStr != null){
+            mShareActionProvider.setShareIntent(createShareForestIntent());
+        } else{
+            Log.d(LOG_TAG, "ShareAction provider is null!");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        Log.v(LOG_TAG, "onCreateView");
+
+//            Intent intent = getActivity().getIntent();
+//            if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+//                mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+//                mForecastStr = intent.getDataString();
+//                Log.v(LOG_TAG, mForecastStr);
+//                detailText = (TextView) rootView.findViewById(R.id.show_detail_text);
+//            }
+        detailText = (TextView) rootView.findViewById(R.id.day_week_text);
+
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        getLoaderManager().initLoader(LOADER_DETAIL, null, this);
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    private Intent createShareForestIntent(){
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, mForecastStr + FORECAST_SHARE_HASHTAG);
+        return shareIntent;
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        Log.v(LOG_TAG, "onCreateLoader");
+        Intent intent = getActivity().getIntent();
+
+        if(intent == null){
+            return  null;
+        }
+
+        return new CursorLoader(getActivity(), intent.getData(), FORECAST_COLUMNS, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        Log.v(LOG_TAG, "onLoadFinished");
+        if(!data.moveToFirst()) {
+            return;
+        }
+
+        String dateString = Utility.formatDate(data.getLong(COL_WEATHER_DATE));
+        String weatherDesc = data.getString(COL_WEATHER_DESC);
+
+        boolean isMetric = Utility.isMetric(getActivity());
+
+        String max = Utility.formatTemperature(getContext(), data.getDouble(COL_WEATHER_MAX_TEMP), isMetric);
+        String min = Utility.formatTemperature(getContext(), data.getDouble(COL_WEATHER_MIN_TEMP), isMetric);
+
+        mForecastStr = String.format("%s - %s - %s/%s", dateString, weatherDesc, max, min);
+        detailText.setText(mForecastStr);
+
+        if(mShareActionProvider != null){
+            mShareActionProvider.setShareIntent(createShareForestIntent());
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
+}

0b36add21a79bab925d8b63657505177711b9aea
donato <ddutra9@gmail.com>
2017-07-18 09:53:45 -0300

    detail fragment implementada falta corrigir

--- app/src/main/java/com/ddutra9/sunshinenano/DetailFragment.java
+++ app/src/main/java/com/ddutra9/sunshinenano/DetailFragment.java
@@ -28,7 +28,8 @@ import java.text.SimpleDateFormat;
  */
 
 public class DetailFragment extends Fragment  implements LoaderManager.LoaderCallbacks<Cursor>{
-    private TextView dayWeekText, dayMonthText, dayMaxTemp, dayMinTemp;
+    private TextView dayWeekText, dayMonthText, dayMaxTemp, dayMinTemp, humidityText, windSpeedText,
+            pressureText;
     private static final int LOADER_DETAIL = 0;
 
     private static final String[] FORECAST_COLUMNS = {
@@ -36,7 +37,11 @@ public class DetailFragment extends Fragment  implements LoaderManager.LoaderCal
             WeatherContract.WeatherEntry.COLUMN_DATE,
             WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
             WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
-            WeatherContract.WeatherEntry.COLUMN_MIN_TEMP
+            WeatherContract.WeatherEntry.COLUMN_MIN_TEMP,
+            WeatherContract.WeatherEntry.COLUMN_HUMIDITY,
+            WeatherContract.WeatherEntry.COLUMN_WIND_SPEED,
+            WeatherContract.WeatherEntry.COLUMN_PRESSURE,
+            WeatherContract.WeatherEntry.COLUMN_DEGREES
     };
 
     static final int COL_WEATHER_ID = 0;
@@ -44,6 +49,10 @@ public class DetailFragment extends Fragment  implements LoaderManager.LoaderCal
     static final int COL_WEATHER_DESC = 2;
     static final int COL_WEATHER_MAX_TEMP = 3;
     static final int COL_WEATHER_MIN_TEMP = 4;
+    static final int COL_HUMIDITY = 5;
+    static final int COL_WIND_SPEED = 6;
+    static final int COL_PRESSURE = 7;
+    static final int COL_DEGREES = 8;
 
     private static final String LOG_TAG = DetailFragment.class.getSimpleName();
 
@@ -82,6 +91,9 @@ public class DetailFragment extends Fragment  implements LoaderManager.LoaderCal
         dayMonthText = (TextView) rootView.findViewById(R.id.day_month_text);
         dayMaxTemp = (TextView) rootView.findViewById(R.id.max_temp_text);
         dayMinTemp = (TextView) rootView.findViewById(R.id.min_temp_text);
+        humidityText = (TextView) rootView.findViewById(R.id.humidity_text);
+        windSpeedText = (TextView) rootView.findViewById(R.id.wind_text);
+        pressureText = (TextView) rootView.findViewById(R.id.pressure_text);
 
         return rootView;
     }
@@ -135,6 +147,12 @@ public class DetailFragment extends Fragment  implements LoaderManager.LoaderCal
         dayMaxTemp.setText(max);
         dayMinTemp.setText(min);
 
+        windSpeedText.setText(Utility.getFormattedWind(getContext(), data.getFloat(COL_WIND_SPEED),
+                data.getFloat(COL_DEGREES)));
+
+        humidityText.setText(Utility.getFormattedHumidity(getContext(), data.getFloat(COL_HUMIDITY)));
+
+        pressureText.setText(Utility.getFormattedPressure(getContext(), data.getFloat(COL_PRESSURE)));
 
         if(mShareActionProvider != null){
             mShareActionProvider.setShareIntent(createShareForestIntent());

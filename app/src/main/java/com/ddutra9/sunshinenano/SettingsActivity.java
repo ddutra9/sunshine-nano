29fe6ca8605d65a391238598070472b39f672e0f
ddutra9 <7Free787>
2017-09-21 00:23:57 -0300

    add array for custom and pref integration

--- app/src/main/java/com/ddutra9/sunshinenano/SettingsActivity.java
+++ app/src/main/java/com/ddutra9/sunshinenano/SettingsActivity.java
@@ -30,6 +30,7 @@ public class SettingsActivity extends PreferenceActivity
         // updated when the preference changes.
         bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
         bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_units_key)));
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_art_pack_key)));
     }
 
     /**
@@ -111,6 +112,8 @@ public class SettingsActivity extends PreferenceActivity
             // our location status has changed.  Update the summary accordingly
             Preference locationPreference = findPreference(getString(R.string.pref_location_key));
             bindPreferenceSummaryToValue(locationPreference);
+        } else if ( key.equals(getString(R.string.pref_art_pack_key)) ) {
+            getContentResolver().notifyChange(WeatherContract.WeatherEntry.CONTENT_URI, null);
         }
     }
 
